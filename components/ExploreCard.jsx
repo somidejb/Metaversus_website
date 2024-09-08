046e4b2cc061765c133d067981e1d9815ce08445
somidejb <jbsomide@gmail.com>
2024-09-08 02:40:06 -0600

    fixed deployment bug

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -1,4 +1,5 @@
 'use client';
+
 import { motion } from 'framer-motion';
 import styles from '../styles';
 import { fadeIn } from '../utils/motion';
@@ -18,15 +19,12 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
       className="absolute w-full h-full object-cover rounded-[24px]"
     />
     {active !== id ? (
-      <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute
-      z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0]">
+      <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0]">
         {title}
       </h3>
     ) : (
-      <div className="absolute bottom-0 p-8 justify-start w-full flex-col 
-      bg-[rgba(0,0,0,0.5)] rounded-b-[24px]">
-        <div className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px]
-        glassmorphism mb-[16px]`}>
+      <div className="absolute bottom-0 p-8 justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]">
+        <div className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}>
           <img
             src="/headset.svg"
             alt="headset"
